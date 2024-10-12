9cb35f42f4dabeabd7e74b9924b238956aedeb9a
Degarege Nibret <degarege3003nibret@gmail.com>
2024-10-13 01:31:15 +0300

    folder structure updated and dvc intiated

---
+++ scripts/crud.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+import modelss, schemas
+
+def get_detection_result(db: Session, detection_id: int):
+    return db.query(modelss.DetectionResult).filter(modelss.DetectionResult.id == detection_id).first()
+
+def get_detection_results(db: Session, skip: int = 0):
+    return db.query(modelss.DetectionResult).offset(skip).all()
+
+def create_detection_result(db: Session, detection: schemas.DetectionResultCreate):
+    db_detection = modelss.DetectionResult(**detection.dict())
+    db.add(db_detection)
+    db.commit()
+    db.refresh(db_detection)
+    return db_detection
+
+def update_detection_result(db: Session, detection_id: int, detection: schemas.DetectionResultCreate):
+    db_detection = get_detection_result(db, detection_id)
+    if db_detection is None:
+        return None
+    for key, value in detection.dict().items():
+        setattr(db_detection, key, value)
+    db.commit()
+    db.refresh(db_detection)
+    return db_detection
+
+def delete_detection_result(db: Session, detection_id: int):
+    db_detection = get_detection_result(db, detection_id)
+    if db_detection is None:
+        return None
+    db.delete(db_detection)
+    db.commit()
+    return db_detection
---
+++ scripts/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+class DetectionResultBase(BaseModel):
+    image_id: str
+    x_min: float
+    y_min: float
+    x_max: float
+    y_max: float
+    confidence: float
+    class_id: int
+    class_name: str
+class DetectionResultCreate(DetectionResultBase):
+    pass
+
+class DetectionResult(DetectionResultBase):
+    id: int
+
+    class Config:
+        from_attributes = True
