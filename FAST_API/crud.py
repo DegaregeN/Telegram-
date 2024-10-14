fe77ff1c4bed7319af755af0b440f711c0512093
Degarege Nibret <degarege3003nibret@gmail.com>
2024-10-15 00:55:33 +0300

    interface

---
+++ FAST_API/crud.py
@@ -0,0 +1,25 @@
+
+# crud.py
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_item(db: Session, item_id: int):
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
+
+def create_item(db: Session, item: schemas.ItemCreate):
+    db_item = models.Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def get_items(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+def delete_item(db: Session, item_id: int):
+    db_item = get_item(db, item_id)
+    if db_item:
+        db.delete(db_item)
+        db.commit()
+        return True
+    return False
